85e50f6727ac4637e9fd7ff79deebe6d7e2c8292
Nov Matake <nov@matake.jp>
2018-08-29 14:02:14 +0900

    init

---
+++ lib/web_authn.rb
@@ -0,0 +1,5 @@
+require "web_authn/version"
+
+module WebAuthn
+  # Your code goes here...
+end
---
+++ spec/web_authn_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe WebAuthn do
+  it "has a version number" do
+    expect(WebAuthn::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
